f776c949c36b81340ae6be0da91490b8f764283d
Or Gabay <orgabay@gmail.com>
2017-10-02 18:53:31 +0300

    Add statistics grapsh Add joi query Add proper init function to add data

--- WebApplication1/Scripts/shauli.js
+++ WebApplication1/Scripts/shauli.js
@@ -126,3 +126,108 @@ if ($(location).attr('pathname') === '/Blog/About') {
     }
 
 }
+
+/* Statistics page functionallity */
+if ($(location).attr('pathname') === '/Blog/Statistics') {
+    $(document).ready(function () {
+        /* Posts per user pie initialization */
+        $('aside').css('display', 'inline-block');
+
+        var canvas = document.querySelector("canvas"),
+            context = canvas.getContext("2d");
+
+        var width = canvas.width,
+            height = canvas.height,
+            radius = Math.min(width, height) / 2;
+
+        var colors = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"];
+
+        var arc = d3.arc()
+            .outerRadius(radius - 10)
+            .innerRadius(0)
+            .context(context);
+
+        var labelArc = d3.arc()
+            .outerRadius(radius - 40)
+            .innerRadius(radius - 40)
+            .context(context);
+
+        var pie = d3.pie()
+            .sort(null)
+            .value(function (d) { return d.Posts; });
+
+        context.translate(width / 2, height / 2);
+
+        d3.json("http://localhost:5242/Blog/Statistics?publisher=True", function (error, data) {
+            if (error) throw error;
+
+            var arcs = pie(data);
+
+            arcs.forEach(function (d, i) {
+                context.beginPath();
+                arc(d);
+                context.fillStyle = colors[i];
+                context.fill();
+            });
+
+            context.beginPath();
+            arcs.forEach(arc);
+            context.strokeStyle = "#fff";
+            context.stroke();
+
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.fillStyle = "#000";
+            arcs.forEach(function (d) {
+                var c = labelArc.centroid(d);
+                context.fillText(d.data.Name + " - " + d.data.Posts, c[0], c[1]);
+            });
+        });
+
+
+        /* Posts per data bar-chart initialization */
+        var svg = d3.select("svg"),
+            margin = { top: 20, right: 20, bottom: 30, left: 40 },
+            width = +svg.attr("width") - margin.left - margin.right,
+            height = +svg.attr("height") - margin.top - margin.bottom;
+
+        var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+            y = d3.scaleLinear().rangeRound([height, 0]);
+
+        var g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.json("http://localhost:5242/Blog/Statistics?dates=True", function (error, data) {
+            if (error) throw error;
+
+            x.domain(data.map(function (d) { return d.Date; }));
+            y.domain([0, d3.max(data, function (d) { return d.Count; })]);
+
+            g.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+
+            g.append("g")
+                .attr("class", "axis axis--y")
+                .call(d3.axisLeft(y).ticks(10))
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "0.71em")
+                .attr("text-anchor", "end")
+
+            g.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function (d) { return x(d.Date); })
+                .attr("y", function (d) { return y(d.Count); })
+                .attr("width", x.bandwidth())
+                .attr("height", function (d) { return height - y(d.Count); });
+        });
+
+
+
+    })
+}
