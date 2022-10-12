5508287d464e60efa69c79fbb7228663b805a916
orgabay12 <32264271+orgabay12@users.noreply.github.com>
2022-10-12 13:15:28 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+  backend "s3"{
+    region = "us-west-2"
+    bucket = "tf-gabay"
+  }
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_s3_bucket" "barak-tf" {
+  bucket = "mye-tf-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "barak-tf-acl" {
+  bucket = aws_s3_bucket.barak-tf.id
+  acl    = "private"
+}
